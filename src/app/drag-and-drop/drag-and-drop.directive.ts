1bbcfc76ac2dc54b559f11b857da3930b1e04a9d
Bart Vets <bart.vets@qframe.be>
2017-10-28 21:47:40 +0200

    adding bootstrap

---
+++ src/app/drag-and-drop/drag-and-drop.directive.ts
@@ -0,0 +1,9 @@
+import { Directive, Input, ElementRef, OnInit, OnChanges, SimpleChange } from '@angular/core';
+
+@Directive({selector: '[dragAndDrop]'})
+export class DragAndDropDirective implements OnInit {
+        
+    ngOnInit(): void {
+
+    }
+}
